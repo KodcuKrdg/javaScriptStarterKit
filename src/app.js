fa05e37f2e99d655ee98999bececf5fc176518b8
KodcuKrdg <ibrahim.krdg98@hotmail.com>
2021-10-24 23:25:38 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba ibrahim")
+
+let dolarDun = 9.2
+let dolarBugun = 9.3
+
+{
+    let dolarDun = 9.1
+}
+
+console.log(dolarDun)
+
+const euroDun = 11
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0; i < konutKredileri.length; i++){
+    console.log("<li>" + konutKredileri[i] + "</li>")
+    
+}
+console.log("</ul>")
